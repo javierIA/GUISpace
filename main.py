91d770364085599a66579605cda31b3a7e99fed9
Javier Flores <81644684+javierIA@users.noreply.github.com>
2022-10-19 15:39:02 -0600

    Razon perfecta

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pystray
 from runtime import execute
 from pystray import Menu, MenuItem as item
 from PIL import Image
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
